ac74d93423d7b96534bf2351d8b3db088cb570e1
Steven McCoy <fnjordy@gmail.com>
2015-11-16 02:54:55 +0000

    Convert to ES6 class.

--- htdocs/poll.js
+++ htdocs/poll.js
@@ -1,71 +1,113 @@
 // Whole-script strict mode syntax
 "use strict";
-
-let sock = undefined;
-let poll_id = undefined, retry_id = undefined;
-let sock_onopen = function() {
-	document.getElementById("status").textContent = "connected";
-	sock_poll();
-};
-let sock_onclose = function(e) {
-	document.getElementById("status").textContent = "disconnected";
-	if (typeof poll_id === "number") {
-		window.clearTimeout(poll_id);
-		poll_id = undefined;
+class KigoronPoller {
+	constructor(url) {
+		this.url = url;
+		this.sock = undefined;
+		this.poll_id = undefined;
+		this.retry_id = undefined;
 	}
-	if (!e.wasClean) {
-		let timeout = 1000;
+
+	Connect() {
+		let new_sock = new WebSocket(this.url);
+		new_sock.onopen = (e) => this.OnOpen(e);
+		new_sock.onclose = (e) => this.OnClose(e);
+		new_sock.onmessage = (e) => this.OnMessage(e);
+		this.sock = new_sock;
+	}
+
+	OnOpen() {
+		document.getElementById("status").textContent = "connected";
+		this.SchedulePoll();
+	}
+
+	Close() {
+		if (typeof this.retry_id === "number") {
+			window.clearTimeout(this.retry_id);
+			this.retry_id = undefined;
+		}
+		this.CancelPoll();
+		if (this.sock !== undefined) {
+			this.sock.close();
+			this.sock = undefined;
+		}
+	}
+
+	OnClose(e) {
+		this.CancelPoll();
+		if (!e.wasClean) {
+			document.getElementById("status").textContent = "disconnected";
+			let timeout = 1000;
+			if (!document.hasFocus()) {
+				timeout *= 2;
+			}
+			this.retry_id = window.setTimeout(() => this.Connect(), timeout);
+		}
+	}
+
+	SchedulePoll() {
+		let timeout = 100;
 		if (!document.hasFocus()) {
 			timeout *= 2;
 		}
-		retry_id = window.setTimeout(function() {
-			sock = sock_connect();
-		}, timeout);
+		this.poll_id = window.setTimeout(() => this.SendPoll(), timeout);
 	}
-};
-let sock_poll = function() {
-	let timeout = 100;
-	if (!document.hasFocus()) {
-		timeout *= 2;
+
+	CancelPoll() {
+		if (typeof this.poll_id === "number") {
+			window.clearTimeout(this.poll_id);
+			this.poll_id = undefined;
+		}
 	}
-	poll_id = window.setTimeout(function() {
-		if (sock.readyState == WebSocket.OPEN) {
-			sock.send("!");
+
+	SendPoll() {
+		if (this.sock.readyState !== WebSocket.OPEN)
+			return;
+		if (this.sock.bufferedAmount === 0)
+			this.sock.send("!");
+	}
+
+	OnMessage(e) {
+		let msg = JSON.parse(e.data);
+		window.requestAnimationFrame(() => this.OnUpdate(msg));
+		this.SchedulePoll();
+	}
+
+	OnUpdate(msg) {
+		document.getElementById("hostname").textContent = msg.hostname;
+		document.getElementById("username").textContent = msg.username;
+		document.getElementById("pid").textContent = msg.pid;
+		document.getElementById("clients").textContent = msg.clients;
+	}
+
+	OnHidden() {
+		document.getElementById("status").textContent = "paused";
+		this.CancelPoll();
+	}
+
+	OnVisible() {
+		if (this.poll_id === undefined &&
+			this.sock !== undefined &&
+			this.sock.readyState === WebSocket.OPEN)
+		{
+			this.OnOpen();
 		}
-	}, timeout);
+	}
 }
-let sock_onerror = function(e) {
-	console.error(e);
-}
-let sock_onmessage = function(e) {
-	let msg = JSON.parse(e.data);
-	document.getElementById("hostname").textContent = msg.hostname;
-	document.getElementById("username").textContent = msg.username;
-	document.getElementById("pid").textContent = msg.pid;
-	document.getElementById("clients").textContent = msg.clients;
-	sock_poll();
-};
-let sock_connect = function() {
-	let new_sock = new WebSocket("ws://" + window.location.host + "/ws");
-	new_sock.onopen = sock_onopen;
-	new_sock.onclose = sock_onclose;
-	new_sock.onmessage = sock_onmessage;
-	return new_sock;
-};
-(function() {
-	sock = sock_connect();
-	document.addEventListener("visibilitychange", function() {
-		switch (document.visibilityState) {
-		case "hidden":
-		case "unloaded":
-			document.getElementById("status").textContent = "paused";
-			sock.onclose({ wasClean: true });
-			break;
-		case "visible":
-			if (poll_id === undefined && sock.readyState == WebSocket.OPEN) {
-				sock.onopen();
-			}
-			break;
-		}
-	});
-})();
+
+let poller = new KigoronPoller("ws://" + window.location.host + "/ws");
+poller.Connect();
+
+document.addEventListener("visibilitychange", function() {
+	switch(document.visibilityState) {
+	case "hidden":
+		poller.OnHidden();
+		break;
+	case "unloaded":
+		poller.Close();
+		break;
+	case "visible":
+		poller.OnVisible();
+		break;
+	}
+});
